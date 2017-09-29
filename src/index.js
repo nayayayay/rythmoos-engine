85e10eb42dac6be6bd3199abcd71c27abbd7c441
ChibiFR <23081995b@gmail.com>
2017-09-29 22:11:43 +0200

    Add eventhandler def

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@
  * @typedef {string|CanvasPattern|CanvasGradient} CanvasColour
  */
 
+/**
+ * @callback EventHandler
+ * @param {Event} event The event object emitted by the browser.
+ */
+
 export * from './containers';
 export * from './geometry';
 export * from './graphics';
+export * from './inputs';
 export * from './rendering';
 export * from './timing';
