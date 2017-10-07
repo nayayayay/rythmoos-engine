1541ae0eec8659d2b8ff2321eec27d6b2c7439f2
ChibiFR <23081995b@gmail.com>
2017-10-07 08:13:09 +0200

    Add context settings property

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -5,6 +5,8 @@ import {Scene} from './Scene';
  * Any game instance comes bundled with a renderer already.
  */
 export class Renderer {
+  /** The game's background colour. Defaults to black (#000000). */
+  public backgroundColour: string;
   private _context: CanvasRenderingContext2D;
 
   /**
@@ -12,7 +14,17 @@ export class Renderer {
    * @param context The context of the canvas to render to.
    */
   constructor(context: CanvasRenderingContext2D) {
+    this.backgroundColour = '#000000';
     this._context = context;
+  }
+
+  /**
+   * Set properties to apply to the context everytime the render method is called.<br>
+   * It could for example be used to apply a default fillStyle or a global composite
+   * operation to all your game objects.
+   * @param context The renderer's context, automatically passed in.
+   */
+  public contextProperties(context: CanvasRenderingContext2D): void {
   }
 
   /**
@@ -20,8 +32,15 @@ export class Renderer {
    * @param scene The scene to render.
    */
   public render(scene: Scene): void {
-    this._context.fillStyle = '#000000';
+    this._context.fillStyle = this.backgroundColour;
     this._context.fillRect(0, 0, this._context.canvas.width, this._context.canvas.height);
-    scene._runRender(this._context);
+
+    this.contextProperties(this._context);
+
+    for (const gameObject of scene.getAll()) {
+      this._context.save();
+      gameObject.render(this._context);
+      this._context.restore();
+    }
   }
 }
